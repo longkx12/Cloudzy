c2d6ffdbba81917e4deda6cfbc0b361268379aef
Long <longvkl220203@gmail.com>
2025-03-20 22:00:13 +0700

    update database

--- Cloudzy/Models/Domain/Category.cs
+++ Cloudzy/Models/Domain/Category.cs
@@ -9,8 +9,6 @@ public partial class Category
 
     public string CategoryName { get; set; } = null!;
 
-    public string? CategoryImg { get; set; }
-
     public string? Description { get; set; }
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
